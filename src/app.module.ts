a2e8b6e03b71c94062695889989ca6ed3e0ec216
Kagami <shimura.kyohei7617@gmail.com>
2023-12-10 15:06:10 +0900

    feat: db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 25432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
