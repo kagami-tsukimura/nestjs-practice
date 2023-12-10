3764c04cf6edb6bd68800c22a239ad95ade05ddc
Kagami <shimura.kyohei7617@gmail.com>
2023-12-10 11:13:26 +0900

    feat: get post

---
+++ src/items/items.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
+import { ItemsService } from './items.service';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
+  }
+}
