f7f4602694f1c25dbb5247341a05f87556eeb1b8
Kagami <shimura.kyohei7617@gmail.com>
2023-12-12 21:46:54 +0900

    feat: password hash

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import { User } from 'src/entities/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
@@ -5,7 +6,9 @@ import { EntityRepository, Repository } from 'typeorm';
 export class UserRepositry extends Repository<User> {
   async createUser(CreateUserDto): Promise<User> {
     const { username, password, status } = CreateUserDto;
-    const user = this.create({ username, password, status });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashPassword, status });
 
     await this.save(user);
     return user;
