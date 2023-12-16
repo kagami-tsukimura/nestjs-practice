e2223d766737a3d5ff5750a1731694a5e617f57f
Kagami <shimura.kyohei7617@gmail.com>
2023-12-16 14:22:05 +0900

    feat: normal testing

--- src/items/example.spec.ts
+++ src/items/example.spec.ts
@@ -4,10 +4,4 @@ describe('Practice Jest', () => {
     const expected = 1;
     expect(result).toEqual(expected);
   });
-
-  it('test2', () => {
-    const result = 2;
-    const expected = 2;
-    expect(result).toEqual(expected);
-  });
 });
---
+++ src/items/items.service.spec.ts
@@ -0,0 +1,36 @@
+import { Test } from '@nestjs/testing';
+import { ItemRepository } from './item.repository';
+import { ItemsService } from './items.service';
+
+const mockItemRepository = () => ({
+  find: jest.fn(),
+});
+
+describe('ItemsServiceTest', () => {
+  let itemsService;
+  let itemRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        ItemsService,
+        {
+          provide: ItemRepository,
+          useFactory: mockItemRepository,
+        },
+      ],
+    }).compile();
+    itemsService = module.get<ItemsService>(ItemsService);
+    itemRepository = module.get<ItemRepository>(ItemRepository);
+  });
+
+  describe('findAll', () => {
+    it('正常系', async () => {
+      const expected = [];
+      itemRepository.find.mockResolvedValue(expected);
+      const result = await itemsService.findAll();
+
+      expect(result).toEqual(expected);
+    });
+  });
+});
