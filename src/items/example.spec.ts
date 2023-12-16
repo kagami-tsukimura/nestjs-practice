9c8ddcbd3255662603469c03b2b0902a2bfb4c04
Kagami <shimura.kyohei7617@gmail.com>
2023-12-16 12:34:21 +0900

    feat: jest example

---
+++ src/items/example.spec.ts
@@ -0,0 +1,13 @@
+describe('Practice Jest', () => {
+  it('test1', () => {
+    const result = 1;
+    const expected = 1;
+    expect(result).toEqual(expected);
+  });
+
+  it('test2', () => {
+    const result = 2;
+    const expected = 2;
+    expect(result).toEqual(expected);
+  });
+});
